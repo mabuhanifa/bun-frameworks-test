ca454051729f6284f79bc32ee50d91f68413a4ce
mabuhanifa <moh.abuhanifa@gmail.com>
2023-09-14 11:46:47 +0600

    api working

--- hi-elysia/src/index.ts
+++ hi-elysia/src/index.ts
@@ -1,6 +1,11 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/search", ({ query }) => {
+    console.log(query);
+    return `Hello Elysia ${query.q}`;
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
