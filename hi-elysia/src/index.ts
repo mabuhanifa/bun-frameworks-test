eb7df64e3d4e97dd38cb19ecbd100e645c691c5a
mabuhanifa <moh.abuhanifa@gmail.com>
2023-09-14 12:03:30 +0600

    app structure changed

--- hi-elysia/src/index.ts
+++ hi-elysia/src/index.ts
@@ -1,11 +1,15 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia()
-  .get("/search", ({ query }) => {
-    console.log(query);
-    return `Hello Elysia ${query.q}`;
-  })
-  .listen(3000);
+const PORT = Number(process.env.PORT || 3000);
+
+const app = new Elysia();
+
+app.get("/search", ({ query }) => {
+  console.log(query);
+  return `Hello Elysia ${query.q}`;
+});
+
+app.listen(PORT);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
