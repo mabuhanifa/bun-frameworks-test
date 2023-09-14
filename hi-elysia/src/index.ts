933eaebc58c460a8ecd2139e7973f291c93d23a1
mabuhanifa <moh.abuhanifa@gmail.com>
2023-09-14 14:24:45 +0600

    user created

--- hi-elysia/src/index.ts
+++ hi-elysia/src/index.ts
@@ -1,13 +1,43 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { prisma } from "./config/db";
 
 const PORT = Number(process.env.PORT || 3000);
 
 const app = new Elysia();
 
-app.get("/search", ({ query }) => {
-  console.log(query);
-  return `Hello Elysia ${query.q}`;
+app.get("/search", () => {
+  return `Hello from Elysia`;
 });
+
+const userSchema = t.Object({
+  email: t.String(),
+  name: t.String(),
+});
+
+app.post(
+  "/user",
+  async ({ body }) => {
+    try {
+      const { email, name } = body; // Destructure email and name from body
+      const user = await prisma.user.create({
+        data: {
+          email,
+          name,
+        },
+      });
+      return user;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  },
+  {
+    body: t.Object({
+      name: t.String(),
+      email: t.String(),
+    }),
+  }
+);
 
 app.listen(PORT, () => {
   console.log(
